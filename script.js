b9d65c0ae2a341e7ada0336d012d5f9e0accdd73
Navya <88023981+navysai1419@users.noreply.github.com>
2023-12-08 13:33:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-//your JS code here. If required.
-function mul(f1,s1) {
-	f1*2=f1;
-	add=f1+s2;
-	return add;
+function mul(f1, s1) {
+  // Multiply the first input by 2
+  let multipliedResult = f1 * 2;
+
+  // Add the second input to the multiplied result
+  let finalResult = multipliedResult + parseInt(s1, 10);
+
+  return finalResult;
 }
-let f1=prompt("enter first input");
-let s1=prompt("enter second input");
-alert(mul(f1,s1));
+
+let f1 = prompt("Enter the first input");
+let s1 = prompt("Enter the second input");
+
+alert("The final result is: " + mul(Number(f1), s1));
