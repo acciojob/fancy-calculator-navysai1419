bcc9fa56c7e0d3e1fa33c408ac10f05d0046b742
Navya <88023981+navysai1419@users.noreply.github.com>
2023-12-08 13:30:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function mul(f1,s1) {
+	f1*2=f1;
+	add=f1+s2;
+	return add;
+}
+let f1=prompt("enter first input");
+let s1=prompt("enter second input");
+alert(mul(f1,s1));
